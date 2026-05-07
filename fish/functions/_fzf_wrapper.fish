585c5256ececaacc754aeeeab0af034f43f1ef15
Walker W <walkerw@U-JJR9RWLP-0158.local>
2026-05-07 13:59:22 +0800

    chore: fish / fzf / remap the preview scroll keybindings

--- fish/functions/_fzf_wrapper.fish
+++ fish/functions/_fzf_wrapper.fish
@@ -14,7 +14,7 @@ function _fzf_wrapper --description "Prepares some environment variables before 
         # height=90% leaves space to see the current command and some scrollback, maintaining context of work
         # preview-window=wrap wraps long lines in the preview window, making reading easier
         # marker=* makes the multi-select marker more distinguishable from the pointer (since both default to >)
-        set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
+        set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --bind=ctrl-j:preview-half-page-down,ctrl-k:preview-half-page-up'
     end
 
     fzf $argv
